d127979746dcf1290afc2c6e7b1ef1cd455095d5
EmilieMauduit <103190830+EmilieMauduit@users.noreply.github.com>
2022-04-22 11:15:36 +0200

    SW done

--- calc_tools.py
+++ calc_tools.py
@@ -1,11 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+@author: emauduit
+"""
+
+
+from array import array
 import os
+from types import NoneType
 import scipy.io as sio
 from scipy.io import readsav
 import numpy as np
 from math import *
 
-def find_value(a,l, res=0):
-    """Retourne la valeur ou l'indice de la valeur la plus proche de a dans la liste l, par défaut l'indice."""
+def find_value(a:float,l:array, res=None):
+    """Find the index of the value closest to a in a list l.
+        :param a:
+            Value to find in the list
+        :type a:
+            float
+        :param l:
+            List to search into
+        :type l:
+            array
+        :param res:
+            If not specified, the function returns the index. If res=True, returns the value associated to the index.
+    """
     min=np.abs(l[0]-a)
     indmin=0
     for i in range(1,len(l)):
@@ -13,16 +34,65 @@ def find_value(a,l, res=0):
         if (nmin <= min):
             min=nmin
             indmin=i
-    if res==0 :
+    if res==None :
         return(indmin)
     else :
         return (l[indmin])
 
-def synchro_dist(w,Qp,tsync,Mp,Rp,Ms):
-    """ Calcule la distance à laquelle doit se trouver la planète pour qu'il y ait synchronisationd de l'orbite selon l'âge du système"""
+def synchro_dist(w:float,Qp:float,tsync:float,Mp:float,Rp:float,Ms:float):
+    """ Compute the distance at which a planet should be so that its orbit is synchronised, depending on the age of the system.
+        :param w:
+            orbital period 
+        :type w:
+            float
+        :param Qp:
+            factor of dissipation by tidal effect 
+        :type Qp:
+            float
+        :param tsync:
+            time of synchronization
+        :type tsync:
+            float
+        :param Mp:
+            Mass of the planet [Mjup]
+        :type Mp:
+            float
+        :param Rp:
+            Radius of the planet [Rjup]
+        :type Rp:
+            float
+        :param Ms:
+            Mass of the star [Msun]
+        :type Ms:
+            float
+    """
     G=6.6725985e-11 
     a=9*tsync/(4*w*0.26*Qp)
     b=G*Mp/pow(Rp,3)
     c=pow(Ms/Mp,2)
     res=Rp*pow(a*b*c,1/6)
     return(res)
+
+def calc_vsun(t:float):
+    """ Compute the velocity of the solar wind at 1 AU.
+        :param t:
+            stellar age [yr]
+        :type t:
+            float
+    """
+    tau=2.56e7 #yr
+    vo=3397e3 #m/s
+    vsun=vo/pow((1+(t/tau)),0.4)
+    return(vsun)
+
+def calc_nsun(t:float):
+    """ Compute the electron density of the solar wind at 1 AU.
+        :param t:
+            stellar age [yr]
+        :type t:
+            float
+    """
+    tau=2.56e7 #yr
+    no=1.6e10 #m-3
+    nsun=no/pow((1+(t/tau)),1.5)
+    return(nsun)
--- solarwindmodel.py
+++ solarwindmodel.py
@@ -20,51 +20,195 @@ from classStar import *
 
 from calc_tools import *
 
-def masslossrate(t,R):
-    """ Compute the stellar mass loss rate normalized to the sun"""
-    tau=2.56e7 #yr
+def masslossrate(t:float,R:float):
+    """ Compute the stellar mass loss rate normalized to the sun
+        :param t:
+            stellar age [yr]
+        :type t:
+            float
+        :param R:
+            radius of the planet [m]
+        :type R:
+            float
+    """
     mp=1.660540210e-27 #kg
-    vo=3397e3 #m/s
-    no=1.6e10 #m-3
     do=1.49597870e11 #m
 
-    vsun=vo/pow((1+(t/tau)),0.4)
-    nsun=no/pow((1+(t/tau)),1.5)
+    vsun=calc_vsun(t)
+    nsun=calc_nsun(t)
     M=4*np.pi*(do**2)*nsun*vsun*mp*(R**2)
     return(M)
 
-def parker_velocity(v,d,rc, vc):
-    """Expression of f(v(d))=0 based on Parker's model for SW"""
+
+def parker_velocity(v:float,d:float,rc:float, vc:float):
+    """Expression of f(v(d))=0 based on Parker's model for SW
+        :param v:
+            velocity [m/s]
+        :type v:
+            float
+        :param d:
+            distance between the star and the planet [m]
+        :type d:
+            float
+        :param rc:
+            critical radius above which the solar wind become super-sonic
+        :type rc:
+            float
+        :param vc:
+            critical velocity associated to rc
+        :type vc:
+            float
+    """
     res=((v/vc)**2)-log((v/vc)**2)-log((d/rc)**4)-(4.0/(d/rc))+3.0 
     return(res)
 
-def parker_velocity_p(v,d,rc,vc):
+def parker_velocity_p(v:float,d:float,rc:float,vc:float):
+    """Expression of df(v(d))/dv based on Parker's model for SW
+        :param v:
+            velocity [m/s]
+        :type v:
+            float
+        :param d:
+            distance between the star and the planet [m]
+        :type d:
+            float
+        :param rc:
+            critical radius above which the solar wind become super-sonic
+        :type rc:
+            float
+        :param vc:
+            critical velocity associated to rc
+        :type vc:
+            float
+    """
     res=(2*v/(vc**2))-(2/v)
     return(res)
 
-def parker(pla,star,T,vo,limd):
-    """Compute the velocity and the density of the SW using the Parker model"""
+def calc_temperature(M:float,t:float):
+    """ Adjust the temperature of the corona for a star of given age.
+    
+        :param M:
+            Mass of the star in kg
+        :type M:
+            float
+        :param t:
+            Age of the star in yr
+        :type t:
+            float
+
+    """
+
     kb=1.380658e-23 #J/K
     mp=1.660540210e-27 #kg
+    d=1.49597870700e11 #m
+    G=6.6725985e-11 #N.m^2/kg^2
+
+    vsun=calc_vsun(t)
+    nsun=calc_nsun(t)
+
+    Tini=(1e6*4.6e9)/(t+1e9)
+    vc=sqrt(2*kb*Tini/mp)
+    rc=mp*G*M/(4*kb*Tini)
+    v=optimize.newton(parker_velocity,vsun,parker_velocity_p, args=(d,rc,vc), maxiter=50)
+    if (v <= vsun):
+        while ((v >= (vsun+2e3)) or (v <= (vsun-2e3))): 
+            Tini=Tini+0.005e6
+            vc=sqrt(2*kb*Tini/mp)
+            rc=mp*G*M/(4*kb*Tini)
+            v=optimize.newton(parker_velocity,vsun,parker_velocity_p, args=(d,rc,vc), maxiter=50)
+        if (v <= vsun):
+            while ((v >= (vsun+0.5e3)) or (v <= (vsun-0.5e3))): 
+                Tini=Tini+0.001e6
+                vc=sqrt(2*kb*Tini/mp)
+                rc=mp*G*M/(4*kb*Tini)
+                v=optimize.newton(parker_velocity,vsun,parker_velocity_p, args=(d,rc,vc), maxiter=50)
+            T=Tini
+        else:
+            while ((v >= (vsun+0.5e3)) or (v <= (vsun-0.5e3))): 
+                Tini=Tini-0.001e6
+                vc=sqrt(2*kb*Tini/mp)
+                rc=mp*G*M/(4*kb*Tini)
+                v=optimize.newton(parker_velocity,vsun,parker_velocity_p, args=(d,rc,vc), maxiter=50)
+            T=Tini
+
+    else:
+        while ((v >= (vsun+2e3)) or (v <= (vsun-2e3))): 
+            Tini=Tini-0.005e6
+            vc=sqrt(2*kb*Tini/mp)
+            rc=mp*G*M/(4*kb*Tini)
+            v=optimize.newton(parker_velocity,vsun,parker_velocity_p, args=(d,rc,vc), maxiter=50)
+        if (v <= vsun):
+            while ((v >= (vsun+0.5e3)) or (v <= (vsun-0.5e3))): 
+                Tini=Tini+0.001e6
+                vc=sqrt(2*kb*Tini/mp)
+                rc=mp*G*M/(4*kb*Tini)
+                v=optimize.newton(parker_velocity,vsun,parker_velocity_p, args=(d,rc,vc), maxiter=50)
+            T=Tini
+        else:
+            while ((v >= (vsun+0.5e3)) or (v <= (vsun-0.5e3))): 
+                Tini=Tini-0.001e6
+                vc=sqrt(2*kb*Tini/mp)
+                rc=mp*G*M/(4*kb*Tini)
+                v=optimize.newton(parker_velocity,vsun,parker_velocity_p, args=(d,rc,vc), maxiter=50)
+            T=Tini
+    return(T)
+
+
+
+
+
+
+def parker(pla:Planet,star:Star):
+    """Compute the velocity and the density of the SW using the Parker model
+        :param pla:
+            The planet studied
+        :type pla:
+            Planet
+        :param star:
+            The associated star
+        :type star:
+            Star
+    """
+
+    kb=1.380658e-23 #J/K
+    mp=1.660540210e-27 #kg
+    dua=1.49597870700e11 #m
     G=6.6725985e-11 #N.m^2/kg^2
     M=star.mass
     R=star.radius
     d=pla.stardist
     t=star.age
+    T=calc_temperature(M,t)
     vc=sqrt(2*kb*T/mp)
     rc=mp*G*M/(4*kb*T)
     vorb=sqrt(G*M/d)
-    if (d >= limd):
+    
+    #Warning messages
+    if (d<0.01*dua ) :
+        print('Warning : The Parker model has not been verified for such star-planet distances')
+    if (t <0.7e9):
+        print('Warning : The Parker model is not precise for stars with t<0.7 Gyr')
+    if (0.7e9 <= t <= 1.6e9):
+        dlim=0.01*dua
+    elif ( 1.6e9 < t < 3.5e9):
+        dlim=0.02*dua
+    elif (3.5e9 <= t ):
+        dlim=0.03*dua
+    else :
+        dlim=0.
+        print ('Star age is too small')
+
+    if (d > dlim):
         try:
             v=optimize.newton(parker_velocity,350.0e3,parker_velocity_p, args=(d,rc,vc), maxiter=50)
         except (ZeroDivisionError, RuntimeError):
             v=0.
     else :
         try:
-            v=optimize.newton(parker_velocity,10.0e3,parker_velocity_p, args=(d,rc,vc), maxiter=100)
+            v=optimize.newton(parker_velocity,10e3,parker_velocity_p, args=(d,rc,vc), maxiter=50)
         except (ZeroDivisionError, RuntimeError):
             v=0.
-
+    
     veff=sqrt((v**2)+(vorb**2))
     Mls=masslossrate(t,R)
     n=Mls/(4*np.pi*(d**2)*v*mp)
